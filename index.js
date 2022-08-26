6941af3d8197dd9497dc3ebc57f28f69392f0736
Jose Hernandez <jhernandez@azurian.com>
2022-08-26 15:18:37 -0400

    hj

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 const server = require('http').Server(app)
 // Setup a webhook route
