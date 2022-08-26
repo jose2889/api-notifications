7f639f8e8230528da4815c0d448b8a134afa0796
Jose Hernandez <jhernandez@azurian.com>
2022-08-26 14:56:55 -0400

    Servidor en linea

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const app = express()
 const PORT = process.env.PORT || 3000
 // Setup a webhook route
 app.use(bodyParser.json())
+
+app.get('/ ', (req, res) => {
+    console.log(req.body) // print all response
+  
+    //messageFrom=req.body['data']['from'] // sender number
+    //messageMsg=req.body['data']['body'] // Message text
+    res.send("Servidor en linea!!!")
+  })
+
 app.post('/notifications ', (req, res) => {
   console.log(req.body) // print all response
 
