4ef89a0189214d214a2d61217633dfdca216d8c0
Jose Hernandez <jhernandez@azurian.com>
2022-08-26 15:16:39 -0400

    hj

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const PORT = process.env.PORT || 3000
+
+const server = require('http').Server(app)
 // Setup a webhook route
 app.use(bodyParser.json())
 
@@ -21,5 +23,21 @@ app.post('/notifications ', (req, res) => {
   res.status(200).end()
 })
 
+app.post('/notificationws', (req, res) => {
+    
+    let { message, number } = req.body
+    number = cleanNumber(number);
+    console.log("Enviando al numero ", number)
+    let msj = 
+        `Estimado *${message}* le notificamos que se ha agendado su reserva con exito 🙂🤖\n Para su comodidad se ha enviado una notificación a su correo donde podra gestionar su reserva \n`;
+    
+    client.sendMessage(number, message);
+
+    res.send({ status: 'Ok' })
+}
+)
+
 app.use(bodyParser.json())
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}🚀 `))
+server.listen(port, () => {
+    console.log(`El server esta listo por el puerto ${port}`);
+})
