08d93f874da582b6a409dbad200fb997c4bda8da
Jose Hernandez <jhernandez@azurian.com>
2022-08-26 14:55:09 -0400

    correccion de puerto

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 // Setup a webhook route
 app.use(bodyParser.json())
 app.post('/notifications ', (req, res) => {
